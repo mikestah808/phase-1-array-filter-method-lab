bba3ea8ba6df308551559deaa8289f34e169ddef
Michael Stafford <michaelstafford808@gmail.com>
2021-12-16 10:51:16 -0800

    commit tree 65b8928891cbc055dc95ca08dd9da196dbca0548

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution here
+
+
+
+
+
+function findMatching(drivers, string) {
+    const result = drivers.filter(driversName => driversName.toLowerCase() === string.toLowerCase())
+    return result;
+}
+
+function fuzzyMatch(drivers, string) {
+    const result = drivers.filter(driversName => driversName.startsWith(string))
+    return result;
+}
+
+function matchName(drivers, string) {
+    const result = drivers.filter(driversName => driversName.name === string)
+    return result;
+}
+
+
+
